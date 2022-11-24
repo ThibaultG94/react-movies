ea2c01789e9613fe29d3c12fb9655c49b957e5a4
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-24 17:41:37 +0100

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Card = () => {
-	return <div></div>;
+const Card = ({ movie }) => {
+	return (
+		<li className="card">
+			<img src={movie.backdrop_path} alt={movie.title} />
+		</li>
+	);
 };
 
 export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import Card from '../components/Card';
 
 const Home = () => {
 	const [dataMovies, setDataMovies] = useState([]);
@@ -9,13 +10,18 @@ const Home = () => {
 			'https://api.themoviedb.org/3/search/movie?api_key=71f1c0748cbaa032bf6d4124a879bf21&query=code&language=fr-FR'
 		)
 			.then((res) => res.json())
-			.then((data) => setDataMovies(data))
+			.then((data) => setDataMovies(data.results))
 			.catch((error) => console.log('Erreur : ' + error));
 	}, []);
 
 	return (
 		<div>
 			<Header />
+			<div className="result">
+				{dataMovies.map((movie) => (
+					<Card key={movie.id} movie={movie} />
+				))}
+			</div>
 		</div>
 	);
 };
