d6cf4241436d6b2f6c31366ae374541b0ab56cbb
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-24 17:16:24 +0100

    fetch api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 const Home = () => {
+	const [dataMovies, setDataMovies] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			'https://api.themoviedb.org/3/search/movie?api_key=71f1c0748cbaa032bf6d4124a879bf21&query=code&language=fr-FR'
+		)
+			.then((res) => res.json())
+			.then((data) => setDataMovies(data))
+			.catch((error) => console.log('Erreur : ' + error));
+	}, []);
+
 	return (
 		<div>
 			<Header />
