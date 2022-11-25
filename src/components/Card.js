8469a0316396f1c4e50381d73e0c6d7c4ffdb1f2
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-25 15:04:48 +0100

    genres

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Card = ({ movie }) => {
+	const [dataGenre, setDataGenre] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			'https://api.themoviedb.org/3/genre/movie/list?api_key=71f1c0748cbaa032bf6d4124a879bf21&language=fr-FR'
+		)
+			.then((res) => res.json())
+			.then((data) => setDataGenre(data.genres))
+			.catch((error) => console.log('Erreur : ' + error));
+	}, []);
+
 	return (
 		<li className="card">
 			<img
-				src={`https://image.tmdb.org/t/p/original` + movie.poster_path}
+				src={
+					`https://image.tmdb.org/t/p/original`
+						? `https://image.tmdb.org/t/p/original` +
+						  movie.poster_path
+						: `./img/poster.jpg`
+				}
 				alt={movie.title}
 			/>
 			<h2>{movie.title}</h2>
@@ -15,6 +31,10 @@ const Card = ({ movie }) => {
 			<h4>
 				{movie.vote_average + `/10`} <span>⭐</span>
 			</h4>
+			<ul></ul>
+			<h3>Synopsis</h3>
+			<p>{movie.overview}</p>
+			<div className="btn">Ajouter aux coups de coeur</div>
 		</li>
 	);
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Card from '../components/Card';
 
 const Home = () => {
 	const [dataMovies, setDataMovies] = useState([]);
+	const [dataGenre, setDataGenre] = useState([]);
 
 	useEffect(() => {
 		fetch(
@@ -12,6 +13,13 @@ const Home = () => {
 			.then((res) => res.json())
 			.then((data) => setDataMovies(data.results))
 			.catch((error) => console.log('Erreur : ' + error));
+
+		fetch(
+			'https://api.themoviedb.org/3/genre/movie/list?api_key=71f1c0748cbaa032bf6d4124a879bf21&language=fr-FR'
+		)
+			.then((res) => res.json())
+			.then((data) => setDataGenre(data.genres))
+			.catch((error) => console.log('Erreur : ' + error));
 	}, []);
 
 	return (
@@ -19,7 +27,40 @@ const Home = () => {
 			<Header />
 			<div className="result">
 				{dataMovies.map((movie) => (
-					<Card key={movie.id} movie={movie} />
+					<li className="card" key={movie.id}>
+						<img
+							src={
+								`https://image.tmdb.org/t/p/original`
+									? `https://image.tmdb.org/t/p/original` +
+									  movie.poster_path
+									: `./img/poster.jpg`
+							}
+							alt={movie.title}
+						/>
+						<h2>{movie.title}</h2>
+						<h5>
+							{`Sorti le : ` +
+								movie.release_date
+									.split('-')
+									.reverse()
+									.join('/')}
+						</h5>
+						<h4>
+							{movie.vote_average + `/10`} <span>⭐</span>
+						</h4>
+						<ul>
+							{movie.genre_ids.map((ids) => {
+								return dataGenre
+									.filter((genre) => genre.id === ids)
+									.map((genre) => (
+										<li key={genre.id}>{genre.name}</li>
+									));
+							})}
+						</ul>
+						<h3>Synopsis</h3>
+						<p>{movie.overview}</p>
+						<div className="btn">Ajouter aux coups de coeur</div>
+					</li>
 				))}
 			</div>
 		</div>
