0e59a25380132d24f4d3899a4ee8cccaf4f361ba
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-25 13:35:28 +0100

    card img + title + date + note

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Card = ({ movie }) => {
 	return (
 		<li className="card">
-			<img src={movie.backdrop_path} alt={movie.title} />
+			<img
+				src={`https://image.tmdb.org/t/p/original` + movie.poster_path}
+				alt={movie.title}
+			/>
+			<h2>{movie.title}</h2>
+			<h5>
+				{`Sorti le : ` +
+					movie.release_date.split('-').reverse().join('/')}
+			</h5>
+			<h4>
+				{movie.vote_average + `/10`} <span>⭐</span>
+			</h4>
 		</li>
 	);
 };
