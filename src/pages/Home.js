079e7d3c4eff71e07a9edb836306409b0a3ce153
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-30 14:54:25 +0100

    sort top flop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,73 +1,47 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 
 const Home = () => {
 	const [dataMovies, setDataMovies] = useState([]);
 	const [dataGenre, setDataGenre] = useState([]);
 	const [textInput, setTextInput] = useState('');
-	// const [sortMovies, setSortMovies] = useState('none');
+	const [sortMovies, setSortMovies] = useState('');
 
-	const searchMovies = async (e, sortMovies) => {
-		if (sortMovies == 'goodToBad') {
-			await fetch(
-				'https://api.themoviedb.org/3/search/movie?api_key=71f1c0748cbaa032bf6d4124a879bf21&query=' +
-					textInput +
-					'&language=fr-FR'
-			)
-				.then((res) => res.json())
-				.then((data) => setDataMovies(data.results))
-				.catch((error) => console.log('Erreur : ' + error));
+	const navigate = useNavigate();
 
-			fetch(
-				'https://api.themoviedb.org/3/genre/movie/list?api_key=71f1c0748cbaa032bf6d4124a879bf21&language=fr-FR'
+	const searchMovies = async (e) => {
+		e.preventDefault();
+		document.querySelector('.noresult').innerHTML = ``;
+		await fetch(
+			'https://api.themoviedb.org/3/search/movie?api_key=71f1c0748cbaa032bf6d4124a879bf21&query=' +
+				textInput +
+				'&language=fr-FR'
+		)
+			.then((res) => res.json())
+			.then((data) =>
+				data.results.length !== 0
+					? setDataMovies(data.results)
+					: (document.querySelector(
+							'.noresult'
+					  ).innerHTML = `Aucun résultats`) &&
+					  setDataMovies(data.results)
 			)
-				.then((res) => res.json())
-				.then((data) => setDataGenre(data.genres))
-				.catch((error) => console.log('Erreur : ' + error));
-		} else if (sortMovies === 'badToGood') {
-			await fetch(
-				'https://api.themoviedb.org/3/search/movie?api_key=71f1c0748cbaa032bf6d4124a879bf21&query=' +
-					textInput +
-					'&language=fr-FR'
-			)
-				.then((res) => res.json())
-				.then((data) => setDataMovies(data.results))
-				.catch((error) => console.log('Erreur : ' + error));
+			.catch((error) => console.log('Erreur : ' + error));
 
-			fetch(
-				'https://api.themoviedb.org/3/genre/movie/list?api_key=71f1c0748cbaa032bf6d4124a879bf21&language=fr-FR'
-			)
-				.then((res) => res.json())
-				.then((data) => setDataGenre(data.genres))
-				.catch((error) => console.log('Erreur : ' + error));
-		} else {
-			e.preventDefault();
-			document.querySelector('.noresult').innerHTML = ``;
-			await fetch(
-				'https://api.themoviedb.org/3/search/movie?api_key=71f1c0748cbaa032bf6d4124a879bf21&query=' +
-					textInput +
-					'&language=fr-FR'
-			)
-				.then((res) => res.json())
-				.then((data) =>
-					data.results.length !== 0
-						? setDataMovies(data.results)
-						: (document.querySelector(
-								'.noresult'
-						  ).innerHTML = `Aucun résultats`) &&
-						  setDataMovies(data.results)
-				)
-				.catch((error) => console.log('Erreur : ' + error));
+		fetch(
+			'https://api.themoviedb.org/3/genre/movie/list?api_key=71f1c0748cbaa032bf6d4124a879bf21&language=fr-FR'
+		)
+			.then((res) => res.json())
+			.then((data) => setDataGenre(data.genres))
+			.catch((error) => console.log('Erreur : ' + error));
 
-			fetch(
-				'https://api.themoviedb.org/3/genre/movie/list?api_key=71f1c0748cbaa032bf6d4124a879bf21&language=fr-FR'
-			)
-				.then((res) => res.json())
-				.then((data) => setDataGenre(data.genres))
-				.catch((error) => console.log('Erreur : ' + error));
+		document.querySelector('input[type="text"]').value = '';
+	};
 
-			document.querySelector('input[type="text"]').value = '';
-		}
+	const sortMovie = async (e) => {
+		await setSortMovies(e);
+		navigate('/');
 	};
 
 	return (
@@ -90,55 +64,69 @@ const Home = () => {
 					<div className="btn-sort-container">
 						<div
 							id="goodToBad"
-							onClick={(e) => searchMovies(e, e.target.id)}>
+							onClick={(e) => sortMovie('goodToBad')}>
 							Top <span>&#x2192;</span>
 						</div>
 						<div
 							id="badToGood"
-							onClick={(e) => searchMovies(e, e.target.id)}>
+							onClick={(e) => sortMovie('badToGood')}>
 							Flop <span>&#x2192;</span>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div className="result">
-				{dataMovies.map((movie) => (
-					<li className="card" key={movie.id}>
-						<img
-							src={
-								movie.poster_path
-									? `https://image.tmdb.org/t/p/original` +
-									  movie.poster_path
-									: `./img/poster.jpg`
-							}
-							alt={movie.title}
-						/>
-						<h2>{movie.title}</h2>
-						<h5>
-							{`Sorti le : ` +
-								movie.release_date
-									.split('-')
-									.reverse()
-									.join('/')}
-						</h5>
-						<h4>
-							{movie.vote_count ? movie.vote_average + `/10` : ''}{' '}
-							<span>⭐</span>
-						</h4>
-						<ul>
-							{movie.genre_ids.map((ids) => {
-								return dataGenre
-									.filter((genre) => genre.id === ids)
-									.map((genre) => (
-										<li key={genre.id}>{genre.name}</li>
-									));
-							})}
-						</ul>
-						<h3>Synopsis</h3>
-						<p>{movie.overview}</p>
-						<div className="btn">Ajouter aux coups de coeur</div>
-					</li>
-				))}
+				{dataMovies
+					.sort((a, b) => {
+						if (sortMovies == 'goodToBad') {
+							return b.vote_average - a.vote_average;
+						} else if (sortMovies == 'badToGood') {
+							return a.vote_average - b.vote_average;
+						} else {
+							console.log('Salut :)');
+						}
+					})
+					.map((movie) => (
+						<li className="card" key={movie.id}>
+							<img
+								src={
+									movie.poster_path
+										? `https://image.tmdb.org/t/p/original` +
+										  movie.poster_path
+										: `./img/poster.jpg`
+								}
+								alt={movie.title}
+							/>
+							<h2>{movie.title}</h2>
+							<h5>
+								{`Sorti le : ` +
+									movie.release_date
+										.split('-')
+										.reverse()
+										.join('/')}
+							</h5>
+							<h4>
+								{movie.vote_count
+									? movie.vote_average + `/10`
+									: ''}{' '}
+								<span>⭐</span>
+							</h4>
+							<ul>
+								{movie.genre_ids.map((ids) => {
+									return dataGenre
+										.filter((genre) => genre.id === ids)
+										.map((genre) => (
+											<li key={genre.id}>{genre.name}</li>
+										));
+								})}
+							</ul>
+							<h3>Synopsis</h3>
+							<p>{movie.overview}</p>
+							<div className="btn">
+								Ajouter aux coups de coeur
+							</div>
+						</li>
+					))}
 			</div>
 			<div className="noresult"></div>
 		</div>
